942e95838c6f50cf186df65913ab94dfc66ec66e
Vinay Malyala <srivinay1432@gmail.com>
2024-11-30 22:56:19 +0530

    add special offers section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
             Discover stylish Nike arrivials, quality comfort, and innovation for your active life.
           </p>
 
-          <Button label="Shop now" iconUrl={arrowRight} />
+          <Button label="Shop now" iconURL={arrowRight} />
 
           <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
             {statistics.map((stat) => (
