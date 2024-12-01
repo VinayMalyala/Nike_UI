812e6eede34b5a335ebe936372ad7a0f8208f1e2
Vinay Malyala <srivinay1432@gmail.com>
2024-12-01 22:39:32 +0530

    working on footer section

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { footerLogo } from '../assets/images'
+import { socialMedia } from '../constants'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="max-container">
+        <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
+          <div className="flex flex-col items-start">
+            <a href="/">
+              <img src={footerLogo} width={150} height={46} alt="" />
+            </a>
+
+            <p className="mt-6 text-base leading-7 font-montserrat text-white-400 sm:max-w-sm">
+              Get shoes ready for the new term at your nearest Nike store.
+              Find Your Perfect Size In Store. Get Rewards
+            </p>
+
+            <div className="flex items-center gap-5 mt-8">
+              {socialMedia.map((icon) => (
+                <div></div>
+              ))}
+            </div>
+          </div>
+        </div>
+    </footer>
   )
 }
 
